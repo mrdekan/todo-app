899115b639703be09469c1d93d945d16924a1981
Yehor <yeros05040504@gmail.com>
2024-01-30 12:01:46 +0200

    Added: examples log

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
     }
 
     if(!localStorage.getItem('lists')){
+        console.log('Examples initialized');
             localStorage.setItem('lists','[{"id":"e126ab75-0383-4c66-b3e3-bd93598fe91a","name":"checkboxNoDelete"},{"id":"b0a5a1a7-58bc-4d31-8d4c-288b8b29eaed","name":"checkboxDelete"},{"id":"6b717887-79a2-400a-970b-d45957ed1af7","name":"selectNoDelete"},{"id":"842e7ebf-c6cd-4d84-9a93-7faf352f8989","name":"selectDelete"}]');
             localStorage.setItem('e126ab75-0383-4c66-b3e3-bd93598fe91a','{"id":"e126ab75-0383-4c66-b3e3-bd93598fe91a","name":"checkboxNoDelete","deleteAfterMarking":false,"type":0,"options":[],"items":[{"value":"abc","state":0},{"value":"cad","state":1},{"value":"asd","state":1},{"value":"dsad sad","state":0},{"value":"asdasd","state":0},{"value":" sadsadasd","state":1}]}');
             localStorage.setItem('6b717887-79a2-400a-970b-d45957ed1af7','{"id":"6b717887-79a2-400a-970b-d45957ed1af7","name":"selectNoDelete","deleteAfterMarking":false,"type":1,"options":[{"name":"1 phase","value":0},{"name":"2 phase","value":1},{"name":"3 phase","value":2}],"items":[{"value":"abc","state":0},{"value":"asd","state":0},{"value":"asd","state":0},{"value":"asd","state":2},{"value":"asd","state":1},{"value":"asd","state":1}]}');
